3e9e205fc2d62360dd8c03096247bb48cd170d91
marcela <marcela.azzem@gmail.com>
2019-07-17 00:29:13 -0400

    actualizando mis servicios

--- app/src/main/java/com/example/myshopping/utils.java
+++ app/src/main/java/com/example/myshopping/utils.java
@@ -1,10 +1,13 @@
 package com.example.myshopping;
 
 public class utils {
-    public static String HOST = "http://192.168.1.101:8000";
+    public static String HOST = "http://192.168.0.16:8000";
     public static String REGISTER_SERVICE =HOST +  "/v1.0/api/user";
     public static String LOGIN_SERVICE =HOST +  "/v1.0/api/login";
     public static String LIST_PRODUCTOS_SERVICE =HOST +  "/v1.0/api/productos";
     public static String REGISTER_PRODUCTOS_SERVICE = HOST +  "/v1.0/api/productos";
+    public static String LIST_USERS =HOST +  "/v1.0/api/user";
+    public static String LIST_PRODUCTOS_USER = HOST + "/v1.0/api/productosUser";
+    public static String DELETE_PRODUCTOS_USER = HOST + "/v1.0/api/productosUser";
     public static String token="";
 }
